668caeda4d2d20d391dbddbad388b259cfef372d
canoi12 <canoimaster@gmail.com>
2016-02-28 14:31:18 -0300

    Motion Tween melhorado(Eu acho)

--- player.lua
+++ player.lua
@@ -9,6 +9,7 @@ function player.load()
     player.xvel = 200
     player.yvel = 600
     player.alpha = 100
+    player.vertices = {x0 = player.x,y0 = player.y,x1 = player.x+player.width,y1 = player.y,x2 = player.x+player.width,y2 = player.y+player.height,x3 = player.x,y3 = player.y+player.height}
 end
 
 function player.limits()
@@ -17,17 +18,24 @@ function player.limits()
 end
 
 function player.update(dt)
+    player.vertices = {x0 = player.x,y0 = player.y,x1 = player.x+player.width,y1 = player.y,x2 = player.x+player.width,y2 = player.y+player.height,x3 = player.x,y3 = player.y+player.height}
     if love.keyboard.isDown("left") then
-        if player.xvel < player.speed then player.xvel = player.xvel + 10 end
-        player.x = player.x - (player.xvel*dt)
+        if player.xvel < player.speed then player.xvel = player.xvel + 20 end
+        player.x = player.x - (player.xvel*dt)     
+        player.vertices.x0 = player.x + 16
+        player.vertices.y1 = player.y + 8
+        player.vertices.x1 = player.x+player.width + 16
     elseif love.keyboard.isDown("right") then
-        if player.xvel < player.speed then player.xvel = player.xvel + 10 end
+        if player.xvel < player.speed then player.xvel = player.xvel + 20 end
         player.x = player.x + (player.xvel*dt)
+        player.vertices.x0 = player.x - 16
+        player.vertices.y0 = player.y + 8
+        player.vertices.x1 = player.x+player.width - 16
     else
         player.height = 32
         player.xvel = 0
     end
-    if player.height > 24 then player.height = player.height-(player.xvel*dt/10) end
+    --if player.height > 24 then player.height = player.height-(player.xvel*dt/10) end
     player.yvel = player.yvel + (gravity*dt)
     player.y = player.y + (player.yvel*dt)
     for i,block in ipairs(blocks) do
@@ -53,7 +61,7 @@ end
 
 function player.draw(dt)
     love.graphics.setColor(255,255,255)
-    love.graphics.rectangle("line",player.x,player.y,player.width,player.height)
+    love.graphics.polygon("line",player.vertices.x0,player.vertices.y0,player.vertices.x1,player.vertices.y1,player.vertices.x2,player.vertices.y2,player.vertices.x3,player.vertices.y3)
     love.graphics.print(player.x,player.x,0)
 end
 
