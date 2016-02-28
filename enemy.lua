dcccd4bc8a3905559e38b849cc3c65b94c02195e
canoi12 <canoimaster@gmail.com>
2016-02-28 15:35:03 -0300

    Aumentado a visão dos inimigos

--- enemy.lua
+++ enemy.lua
@@ -4,21 +4,24 @@ enemy = {}
 function enemy.load()
     limit = game.roomWidth/32-1
     j = 0
-    for i = 10, limit do
-        j = love.math.random(10)
-        if j == 5 then
-            newEnemy = {x = i*32,y = 320,width = 32,height = 32,yvel = 600}
-            table.insert(enemies,newEnemy)
-        end
+    while table.getn(enemies) < 10 do
+        j = love.math.random(game.windowWidth,game.roomWidth)
+        newEnemy = {x = j,y = 320,width = 32,height = 32,yvel = 600}
+        table.insert(enemies,newEnemy)
     end
 end
 
 function enemy.update(dt)
+    while table.getn(enemies) < 10 do
+        j = love.math.random(10,game.roomWidth/32)
+        newEnemy = {x = j*32,y = 320,width = 32,height = 32,yvel = 600}
+        table.insert(enemies,newEnemy)
+    end
     for i,en in ipairs(enemies) do
         if en.y + (en.yvel*dt) < love.graphics:getHeight()-32-en.height then
             en.yvel = en.yvel + (gravity*dt) 
             en.y = en.y + (en.yvel*dt)
-            for i,block in ipairs(blocks) do
+            for j,block in ipairs(blocks) do
                 if checkCol(en.x,en.y+(en.y*dt),en.width,en.height,block.x,block.y,block.width,block.height) then
                         en.yvel = 0
                         en.y = block.y - en.height
@@ -26,7 +29,7 @@ function enemy.update(dt)
                 end
             end
         end
-        if math.sqrt((en.x-player.x)^2+(en.y-player.y)^2) < 180 then
+        if math.sqrt((en.x-player.x)^2+(en.y-player.y)^2) < 320 then
             en.x = en.x + ((player.x-en.x)*dt)
         end
     end
