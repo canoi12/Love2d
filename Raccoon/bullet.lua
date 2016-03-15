24caae29d2176b131e80b6fd6a65fcd9e71d064d
canoi12 <canoimaster@gmail.com>
2016-03-15 14:50:35 +0000

    "Classe" das balas arrumada

--- Raccoon/bullet.lua
+++ Raccoon/bullet.lua
@@ -9,24 +9,23 @@ function bullet.update(dt)
     for i,b in ipairs(bullets) do
         b.x = b.x + (b.dx*dt)
         b.y = b.y + (b.dy*dt)
+        if b.x > camera.x + game.windowWidth or b.x < camera.x or
+           b.y > camera.y + game.windowHeight or b.y < camera.y then
+            table.remove(bullets,i)     
+       end
+        for j,e in ipairs(enemies) do
+            if b.x > e.x and b.x < e.x+e.width and
+                b.y > e.y and b.y < e.y + e.height then
+                table.remove(enemies,j)  
+                table.remove(bullets,i)  
+            end
+        end
     end
 end
 
 function bullet.draw()
     for i,b in ipairs(bullets) do
         love.graphics.circle("line",b.x,b.y,6)
-        if b.x > camera.x + game.windowWidth or b.x < camera.x or
-            b.y > camera.y + game.windowHeight or b.y < camera.y then
-            table.remove(bullets,i)     
-       end
-       
-       for j,e in ipairs(enemies) do
-            if b.x > e.x and b.x < e.x+e.width and
-                b.y > e.y and b.y < e.y + e.height then
-                table.remove(enemies,j)  
-                table.remove(bullets,i)  
-            end
-       end
     end
 end
 
